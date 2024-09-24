b4155aeb069602a9bc3109437b9faf6226736a9b
Pablo Ramon <ramonparaiba@gmail.com>
2024-09-24 07:18:30 -0300

    New: Variaveis Sistema e Variaveis operacoes

---
+++ 004-variaveis-sistema.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Seu diretorio é: $HOME"
+echo "Você está logado como: $USER"
+echo "Os diretórios no seu PATH são: $PATH"
---
+++ 005-variaveis-operacoes.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Definindo variáveis numéricas
+x=10
+y=20
+
+# Somando as variáveis
+soma=$((x + y))
+echo "A soma de $x e $y é: $soma"
+
+#Faça as outras operações aritméticas
