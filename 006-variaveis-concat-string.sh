d0664fa1166c297c88d0ffc5b5e2e87a8f9dc3ca
Pablo Ramon <ramonparaiba@gmail.com>
2024-09-24 07:28:25 -0300

    new: Concatenar string e Receber argumentos

---
+++ 006-variaveis-concat-string.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Definindo variáveis de string
+primeiro_nome="Pablo"
+ultimo_nome="Pinheiro"
+
+# Concatenando strings
+nome_completo="$primeiro_nome $ultimo_nome"
+echo "Nome completo: $nome_completo"
+
+
+### Exercicio
+## Crie variáveis para o endereço, Rua, Numero, Bairro, Cidade, Estado
+## Exiba o endereço completo
---
+++ 007-variaveis-recebendo-argumentos.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+# Exibindo os parâmetros passados ao script
+echo "Primeiro parâmetro: $1"
+echo "Segundo parâmetro: $2"
+
+#para rodar ./meu_script.sh 10 20
